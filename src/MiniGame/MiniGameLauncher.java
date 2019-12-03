693022532e5f5aeb7a570f38a35f107515e4408b
FS <florin.schwappach@ur.de>
2019-12-03 16:28:50 +0100

    new U08, added new version of minigame

---
+++ src/MiniGame/MiniGameLauncher.java
@@ -0,0 +1,11 @@
+package MiniGame;
+
+import de.ur.mi.oop.launcher.GraphicsAppLauncher;
+
+
+public class MiniGameLauncher {
+
+    public static void main(String[] args) {
+        GraphicsAppLauncher.launch("MiniGame");
+    }
+}
